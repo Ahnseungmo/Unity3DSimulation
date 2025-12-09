6fb1b9fc60c988ad6504e7b67d3b59ac0c428acf
Ahnseungmo <43835809+Ahnseungmo@users.noreply.github.com>
2025-12-09 17:47:52 +0900

    애니메이션, 모데링 작업중

--- Assets/Scripts/GameSceneManager.cs
+++ Assets/Scripts/GameSceneManager.cs
@@ -1,14 +1,22 @@
+using Steamworks;
+using Steamworks.Data;
+using System.Threading.Tasks;
 using Unity.Netcode;
 using UnityEngine;
 
 public class GameSceneManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private async Task Start()
     {
+        //////////////디버깅용 호스트 생성 빌드시 삭제/////////////
+        var lobby = await SteamMatchmaking.CreateLobbyAsync(4);
+        lobby.Value.SetData("name", "asdf");
+        NetworkManager.Singleton.StartHost();
+        Debug.Log("Host started on Steam P2P");
+        ///////////////////////////////////////
 
-
-//        NetworkManager.Singleton.SpawnManager
+  
     }
 
     // Update is called once per frame
